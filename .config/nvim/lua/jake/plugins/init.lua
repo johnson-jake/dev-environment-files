b9d5c98001647a6f6f7f07b2ece8e20ec19bb18a
Jake Johnson <jake.johnson@vivint.com>
2024-08-22 09:31:59 -0600

    Adding rustaceanvim

--- .config/nvim/lua/jake/plugins/init.lua
+++ .config/nvim/lua/jake/plugins/init.lua
@@ -1,4 +1,7 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  'mrcjkb/rustaceanvim',
+  version = '^5', -- Recommended
+  lazy = false, -- This plugin is already lazy
 }
